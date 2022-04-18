19fa4d048af05ee48e7008d83f43be001081b0a8
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-04-18 13:05:21 +0530

    Functions refactored

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,18 +1,17 @@
-import getSitemapPaths from "~/utils/getSitemapPaths";
+import getSitemapPages from "~/utils/getSitemapPages";
 import getTotalCounts from "~/lib/getTotalCounts";
-export default function SitemapPage() {
+export default function SitemapIndexPage() {
   return null;
 }
-
 export async function getServerSideProps({ res }) {
   const details = await getTotalCounts();
   const { totalCategories, totalPublishedPages } = details;
   const { totalPublishedPosts, totalTags, totalUsers } = details;
-  const categoryPaths = getSitemapPaths(totalCategories, "category_sitemap");
-  const tagPaths = getSitemapPaths(totalTags, "tag_sitemap");
-  const postPaths = getSitemapPaths(totalPublishedPosts, "post_sitemap");
-  const pagePaths = getSitemapPaths(totalPublishedPages, "page_sitemap");
-  const authorPaths = getSitemapPaths(totalUsers, "author_sitemap");
+  const categoryPaths = getSitemapPages(totalCategories, "category_sitemap");
+  const tagPaths = getSitemapPages(totalTags, "tag_sitemap");
+  const postPaths = getSitemapPages(totalPublishedPosts, "post_sitemap");
+  const pagePaths = getSitemapPages(totalPublishedPages, "page_sitemap");
+  const authorPaths = getSitemapPages(totalUsers, "author_sitemap");
 
   let sitemapIndex = `<?xml version='1.0' encoding='UTF-8'?>
   <sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -26,13 +25,11 @@ export async function getServerSideProps({ res }) {
      ${postPaths}
      </sitemap>
   </sitemapindex>`;
-
   res.setHeader("Content-Type", "text/xml; charset=utf-8");
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=600, stale-while-revalidate=600"
   );
-
   res.write(sitemapIndex);
   res.end();
   return { props: {} };
--- src/utils/getSitemapPages.js
+++ src/utils/getSitemapPages.js
@@ -1,9 +1,8 @@
-export default function getSitemapPaths(count, path) {
+export default function getSitemapPages(count, path) {
   const items = [];
-  let sitemapPerPage = process.env.NEXT_PUBLIC_ITEM_PER_SITEMAP;
+  let sitemapPerPage = process.env.NEXT_PUBLIC_ITEM_PER_SITEMAP; //1000
   for (let i = 1; i <= Math.ceil(count / sitemapPerPage); i++) {
     let url = `${process.env.NEXT_PUBLIC_FRONTEND_URL}/sitemap/${path}${i}.xml`;
-
     items.push(
       ` 
       <sitemap>
